ce9435ae0087181e2af657bab905d03e161d3b7e
dinaliv <diana.nalivaiko@outlook.com>
2026-08-08 01:48:11 -0700

    Add grinder image and adjust positions

--- src/coffeeshop/Grinder.java
+++ src/coffeeshop/Grinder.java
@@ -1,11 +1,12 @@
 package coffeeshop;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.Timer;
 
 public class Grinder implements ActionListener {
@@ -14,10 +15,16 @@ public class Grinder implements ActionListener {
 	private boolean isGrinding = false;
 	private boolean grindDone = false;
 	private Timer grindTimer;
+	private BufferedImage img;
 
 	public Grinder(double x, double y) {
 		xPos = x;
 		yPos = y;
+		try {
+			img = ImageIO.read(getClass().getResourceAsStream("/assets/Grinder.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void startGrinding() {
@@ -52,28 +59,10 @@ public class Grinder implements ActionListener {
 	}
 
 	public void draw(Graphics2D g2) {
-		int x = (int)(xPos - 60);
-		int y = (int)(yPos - 100);
-
-		// body
-		g2.setColor(Color.WHITE);
-		g2.fillRect(x, y, 120, 200);
-		g2.setColor(Color.BLACK);
-		g2.setStroke(new BasicStroke(2));
-		g2.drawRect(x, y, 120, 200);
-		g2.setStroke(new BasicStroke(1));
-
-		// label
-		g2.setColor(Color.BLACK);
-		g2.setFont(new Font("Arial", Font.PLAIN, 14));
-		g2.drawString("Grinder", x + 22, y + 52);
-
-		// hole at the bottom centre
-		g2.setColor(Color.DARK_GRAY);
-		g2.fillOval(x + 44, y + 148, 32, 16);
-		g2.setColor(Color.BLACK);
-		g2.setStroke(new BasicStroke(1.5f));
-		g2.drawOval(x + 44, y + 148, 32, 16);
-		g2.setStroke(new BasicStroke(1));
+		if (img != null) {
+			g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+			g2.drawImage(img, (int)(xPos - 170), (int)(yPos - 192), 340, 435, null);
+			g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		}
 	}
 }
